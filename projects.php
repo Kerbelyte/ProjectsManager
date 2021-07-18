d1a482131d6e491f34bd4d1089d9de3e17974276
Kerbelyte <80693721+Kerbelyte@users.noreply.github.com>
2021-07-18 09:34:05 +0300

    update projects.php file

--- projects.php
+++ projects.php
@@ -94,7 +94,7 @@ echo '</table>';
 
 ?>
 
-<form method="POST">
+<form class="projects-form" method="POST">
     <label for="name" style="font-size: 16px; color: grey;">Add new project:</label>
     <input style = "margin-left: 5px; margin-right: 5%;" class="project-name" type="text" id="name" name="project_name" value="" placeholder="Project name">
     <label style="margin-right: 5px; font-size: 16px; color: grey;" class="employee-name" for="name">Employee name:</label>
